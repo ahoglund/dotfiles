bd78eb5175e2de07d17a5055298e4c7df10cfb08
Andrew Hoglund <ahoglund@github.com>
2021-07-16 17:16:54 -0500

    try zsh

--- install.sh
+++ install.sh
@@ -30,15 +30,15 @@ fi
 
 # Remove the oh-my's
 rm -rf $HOME/.oh-my-zsh
-rm -rf $HOME/.oh-my-bash
+#rm -rf $HOME/.oh-my-bash
 
 # A place for private configs
-touch $HOME/.config/fish/private.fish
-touch $HOME/.bash_private
+#touch $HOME/.config/fish/private.fish
+#touch $HOME/.bash_private
 touch $HOME/.zsh_private
 
-ln -s $dotfiles_dir/fish/functions $HOME/.config/fish/functions
-ln -s $dotfiles_dir/fish/config.fish $HOME/.config/fish/config.fish
+# ln -s $dotfiles_dir/fish/functions $HOME/.config/fish/functions
+# ln -s $dotfiles_dir/fish/config.fish $HOME/.config/fish/config.fish
 
 rm -f $HOME/.tmux.conf
 ln -s $dotfiles_dir/tmux.conf $HOME/.tmux.conf
@@ -51,8 +51,8 @@ ln -s $dotfiles_dir/gitconfig $HOME/.gitconfig
 rm -f $HOME/.gemrc
 ln -s $dotfiles_dir/gemrc $HOME/.gemrc
 
-rm -f $HOME/.bash_profile
-ln -s $dotfiles_dir/bash_profile $HOME/.bash_profile
+# rm -f $HOME/.bash_profile
+# ln -s $dotfiles_dir/bash_profile $HOME/.bash_profile
 
 rm -f $HOME/.zshrc
 ln -s $dotfiles_dir/zshrc $HOME/.zshrc
@@ -65,8 +65,8 @@ ln -s $HOME/.config/nvim $HOME/.vim
 ln -s $dotfiles_dir/hammerspoon/ $HOME/.hammerspoon
 
 # I'd like to use fish, please
-sudo apt-get install -y fish
-sudo chsh -s /usr/bin/fish
+# sudo apt-get install -y fish
+# sudo chsh -s /usr/bin/fish
 
 # Indexing Ruby std-lib
 gem install gem-ctags
@@ -80,7 +80,6 @@ fi
 
 # Install vim plugins
 vim -Es -u $HOME/.vim/init.vim -c "PlugInstall | qa"
-
 
 # Setup GnuPG
 mkdir -p $HOME/.gnupg
