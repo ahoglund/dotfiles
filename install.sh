0b97b3f43555b1b879aae7d052e5e88fd31ccbc5
Andrew Hoglund <ahoglund@github.com>
2021-07-16 16:25:56 -0500

    remove bin

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ dotfiles_dir=$(pwd)
 
 ln -s $HOME/bin $dotfiles_dir/bin
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.config/fish ~/.config/alacritty ~/.config/nvim;
+mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle $HOME/.config/fish $HOME/.config/alacritty $HOME/.config/nvim;
 
 if [ "$(uname -s)" == "Darwin" ]; then
   brew bundle
@@ -32,33 +32,35 @@ fi
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
 
 # Remove the oh-my's
-rm -rf ~/.oh-my-zsh
-rm -rf ~/.oh-my-bash
+rm -rf $HOME/.oh-my-zsh
+rm -rf $HOME/.oh-my-bash
 
 # A place for private configs
-touch ~/.config/fish/private.fish
-touch ~/.bash_private
-touch ~/.zsh_private
+touch $HOME/.config/fish/private.fish
+touch $HOME/.bash_private
+touch $HOME/.zsh_private
 
-ln -s $dotfiles_dir/fish/functions/ ~/.config/fish/functions/
-ln -s $dotfiles_dir/fish/config.fish ~/.config/fish/config.fish
+ln -s $dotfiles_dir/fish/functions/ $HOME/.config/fish/functions/
+ln -s $dotfiles_dir/fish/config.fish $HOME/.config/fish/config.fish
 
 rm -f $HOME/.tmux.conf
-ln -s $dotfiles_dir/tmux.conf ~/.tmux.conf
-ln -s $dotfiles_dir/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s $dotfiles_dir/tmux.conf $HOME/.tmux.conf
+
+ln -s $dotfiles_dir/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 
 mv $HOME/.gitconfig $HOME/.gitconfig.private
 
-ln -s $dotfiles_dir/gitconfig ~/.gitconfig
-ln -s $dotfiles_dir/gemrc ~/.gemrc
-ln -s $dotfiles_dir/bash_profile ~/.bash_profile
-ln -s $dotfiles_dir/zshrc ~/.zshrc
-ln -s $dotfiles_dir/git_template ~/.git_template
+ln -s $dotfiles_dir/gitconfig $HOME/.gitconfig
 
-ln -s $dotfiles_dir/vim/init.vim ~/.vim/init.vim
-ln -s ~/.config/nvim ~/.vim
+ln -s $dotfiles_dir/gemrc $HOME/.gemrc
+ln -s $dotfiles_dir/bash_profile $HOME/.bash_profile
+ln -s $dotfiles_dir/zshrc $HOME/.zshrc
+ln -s $dotfiles_dir/git_template $HOME/.git_template
 
-ln -s $dotfiles_dir/hammerspoon/ ~/.hammerspoon
+ln -s $dotfiles_dir/vim/init.vim $HOME/.vim/init.vim
+ln -s $HOME/.config/nvim $HOME/.vim
+
+ln -s $dotfiles_dir/hammerspoon/ $HOME/.hammerspoon
 
 # I'd like to use fish, please
 apt-get install -y fish
@@ -66,17 +68,17 @@ chsh -s /usr/bin/fish
 
 # Indexing Ruby std-lib
 gem install gem-ctags
-mkdir -p ~/.rbenv/plugins
+mkdir -p $HOME/.rbenv/plugins
 
-if ! [ -d ~/.rbenv/plugins/rbenv-ctags ]; then
+if ! [ -d $HOME/.rbenv/plugins/rbenv-ctags ]; then
   git clone git://github.com/tpope/rbenv-ctags.git \
-    ~/.rbenv/plugins/rbenv-ctags
+    $HOME/.rbenv/plugins/rbenv-ctags
   rbenv ctags
 fi
 
 # Setup GnuPG
-mkdir -p ~/.gnupg
-cd ~/.gnupg && ln -sf $dotfiles_dir/gpg-agent.conf gpg-agent.conf
+mkdir -p $HOME/.gnupg
+cd $HOME/.gnupg && ln -sf $dotfiles_dir/gpg-agent.conf gpg-agent.conf
 
 cd $dotfiles_dir
 # Setup gitconfig
