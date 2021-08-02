3f8a9f0a8ab0dc66ccf45c26124cb80f6c9055a0
Andrew Hoglund <ahoglund@github.com>
2021-08-02 17:03:24 -0500

    fix flavor name

--- install.sh
+++ install.sh
@@ -31,9 +31,7 @@ fi
 
 #install neovim and ctags
 if [ "$os" == "Linux" ]; then
-  if [ "$flavor" == "Ubuntu" ]; then
-    echo "gwell"
-  else
+  if [ "$flavor" == "Debian GNU/Linux" ]; then
     echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
     curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
   fi
