604cbafb30b0ed72d8c7523aed9f851956adfd93
Andrew Hoglund <ahoglund@github.com>
2021-10-19 14:08:58 -0500

    debian 11

--- install.sh
+++ install.sh
@@ -32,8 +32,8 @@ fi
 #install neovim and ctags
 if [ "$os" == "Linux" ]; then
   if [ "$flavor" == "Debian GNU/Linux" ]; then
-    echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
-    curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+    echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+    curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
   fi
 
   sudo apt-get update
