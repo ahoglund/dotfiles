5e1f54ab47a2461585e8e968e03ec2db6b832e35
Andrew Hoglund <ahoglund@github.com>
2023-06-08 14:17:44 -0500

    remove help

--- nvim/lua/kickstart.lua
+++ nvim/lua/kickstart.lua
@@ -242,7 +242,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'ruby', 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim', 'markdown', 'vimdoc' },
+  ensure_installed = { 'ruby', 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'vim', 'markdown', 'vimdoc' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
